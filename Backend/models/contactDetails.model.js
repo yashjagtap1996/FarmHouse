c83228033a201ce08f86430150ef8e3a2b628161
Yash-Likhar <yashlikhar14@gmail.com>
2025-06-17 22:10:42 +0530

    Add files via upload

---
+++ Backend/models/contactDetails.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  purpose: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const ContactDetails = mongoose.model("ContactDetails", contactSchema);
+export default ContactDetails;
