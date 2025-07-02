ead5c7a7f9e4ed93e1f82962fcc38fcd663daf47
Yash-Likhar <yashlikhar14@gmail.com>
2025-07-02 20:57:36 +0530

    Delete Backend directory

--- Backend/routes/contactDetails.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { saveContactDetails } from '../controller/contactDetails.controller.js';
-
-const router = express.Router();
-
-router.post('/',saveContactDetails);
-
-export default router;
