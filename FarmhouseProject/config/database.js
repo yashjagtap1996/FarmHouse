9dac47e760e7f7c6338bba60995abb5e002d8e11
Yash-Likhar <yashlikhar14@gmail.com>
2025-07-02 21:00:00 +0530

    Add files via upload

---
+++ FarmhouseProject/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("database connected"));
+};
+
+export default connectDB;
---
+++ FarmhouseProject/views/services.ejs
@@ -0,0 +1,3 @@
+<div>
+    <h1>Services</h1>
+</div>
