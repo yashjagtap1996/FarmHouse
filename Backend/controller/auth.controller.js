5de31b895a75f2775f54b4f1e315fec70823b6d9
Yash-Likhar <yashlikhar14@gmail.com>
2025-08-13 12:04:39 +0530

    Add files via upload

---
+++ Backend/controller/auth.controller.js
@@ -0,0 +1,7 @@
+import session from "express-session";
+export const checkAuth = async (req, res) => {
+  if (req.session && req.session.user) {
+    return res.json({ loggedIn: true, user: req.session.user });
+  }
+  return res.json({ loggedIn: false });
+};
